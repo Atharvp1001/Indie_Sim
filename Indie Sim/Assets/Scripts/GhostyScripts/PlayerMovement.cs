2b55f26aa060a06e681b9be91d6d735b248f1521
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-07-05 13:13:46 +0530

    Something is remaining

--- Indie Sim/Assets/Scripts/GhostyScripts/PlayerMovement.cs
+++ Indie Sim/Assets/Scripts/GhostyScripts/PlayerMovement.cs
@@ -3,26 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float MoveSpeed;
-
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
     private Rigidbody2D rb;
-    private Vector2 MovementValue;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // Correctly assigning to the class field
+        moveInput = context.ReadValue<Vector2>();
+    }
+
     private void FixedUpdate()
     {
-        rb.linearVelocity = MovementValue * MoveSpeed * Time.deltaTime;
+        // Use velocity or MovePosition as per your desired physics feel
+        rb.linearVelocity = moveInput * moveSpeed;
     }
-
-    private void OnMove(InputValue value)
-    {
-        MovementValue = value.Get<Vector2>();
-    }
-
-
 }
